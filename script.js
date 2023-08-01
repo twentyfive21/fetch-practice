1e7eef4da7b420e0bcb54cc798e254bb63547c55
Tina <tina.winnn@gmail.com>
2023-08-01 18:58:01 -0400

    finsihed adding excercise code

--- script.js
+++ script.js
@@ -3,6 +3,24 @@
 // Link for fetches returns one object remove the /1 to return more 
 // https://jsonplaceholder.typicode.com/todos/1
 
+
+
+
+// ********** THIS IS YOUR ANSWER AREA *************
+
+
+
+
+
+
+
+
+
+
+
+
+// ********* END OF YOUR ANSWER AREA **************
+
 // ~~~~~~~ EXERCISE 1 FETCH ~~~~~~~~~
 
 // fetch(` https://jsonplaceholder.typicode.com/todos/1`)
@@ -21,18 +39,34 @@
 //     console.log(err);
 //   }
 // };
-// You need to call the function or it won't fetch since it is async. Fetch exercise 1 doesn't need to be called since it does it by itself. 
 // fetchData();
+
+// You need to call the function or it won't fetch since it is async. Fetch exercise 1 doesn't need to be called since it does it by itself. 
+
 
 // ~~~~~~~ EXERCISE 3 AXIOS FETCH ~~~~~~~~~
 // For any axios fetch you will need to install the package from 
 // https://www.npmjs.com/package/axios
 // In your terminal type in npm install axios
-// Make sure to import it in the file your doing the fetch and then your all set!
+// Make sure to import it in the file your doing the fetch and then your all set! The import syntax is below. 
+// import axios from 'axios';
+// Another way is also the cdn approach if you look in the index I have it commented as cdn with axios 
 
+// axios.get(`https://jsonplaceholder.typicode.com/todos/1`)
+//   .then(res => console.log(res.data))
+//   .catch(err => console.error(err));
 
-        
 // ~~~~~~~ EXERCISE 4 ASYNC AXIOS FETCH ~~~~~~~~~
+
+// const dataResponse = async () => {
+//     try{
+//         const res = await axios.get(`https://jsonplaceholder.typicode.com/todos/1`);
+//         console.log(res)
+//     }
+//     catch{err => console.log(err)}
+// }
+// dataResponse();
+
 
 
 // ~~ FETCH EXERCISE 5 ASYNC FETCH with header authorization ~~~~
@@ -47,9 +81,7 @@
 //         }
 //       });
 //       const jsonData = await response.json();
-//       // console.log(jsonData)
-//       setPhotos(jsonData?.photos)
-//       setTotalPages(Math.ceil(jsonData?.total_results / 16))
+//       console.log(jsonData)
 //     } catch (err) {
 //       console.error(err);
 //     }
